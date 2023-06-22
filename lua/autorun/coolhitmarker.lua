20b67353aa9d67609da5a8ae20d85b7291929810
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-06-22 11:43:16 +0300

    screenscaling and custom scaling
    requires darsu testing
    shush i know its funny

--- lua/autorun/coolhitmarker.lua
+++ lua/autorun/coolhitmarker.lua
@@ -26,6 +26,7 @@ if SERVER then
             net.WriteBool((ent:IsPlayer() and ent:LastHitGroup() == HITGROUP_HEAD) or ((ent:IsNPC() or ent:IsNextBot()) and npcheadshotted) or false)
             net.WriteBool(bit.band(dmginfo:GetDamageType(), DMG_BURN+DMG_DIRECT) == DMG_BURN+DMG_DIRECT or false)
             net.WriteBool(((ent:IsPlayer() or ent:IsNextBot() or ent:IsNPC()) and ent:Health() <= 0) or (ent:GetNWInt("PFPropHealth", 1) <= 0) or false)
+            net.WriteVector(dmginfo:GetDamagePosition() != Vector() and attacker:VisibleVec(dmginfo:GetDamagePosition()) and dmginfo:GetDamagePosition() or Vector())
             net.WriteUInt((ent:IsPlayer() and (ent:Armor() > 0 and 1 or 0) + (ent.phm_lastArmor > 0 and 2 or 0)) or 0, 2)
             net.WriteUInt(distance, 16)
             net.Send(attacker)
@@ -54,8 +55,8 @@ if SERVER then
             target.phm_lastArmor = target:Armor() or 0
         end
         target.phm_lastHealth = target:Health() or 0
-        if dmginfo:GetAttacker():IsPlayer() and dmginfo:IsDamageType(DMG_BURN) then target.phm_lastAttacker = dmginfo:GetAttacker() end
-        if target.phm_lastAttacker and dmginfo:IsDamageType(DMG_BURN) then
+        if dmginfo:GetAttacker():IsPlayer() and dmginfo:IsDamageType(DMG_BURN+DMG_SLOWBURN) then target.phm_lastAttacker = dmginfo:GetAttacker() end
+        if target.phm_lastAttacker and dmginfo:IsDamageType(DMG_BURN+DMG_SLOWBURN) then
             dmginfo:SetAttacker(target.phm_lastAttacker)
         end
     end)
@@ -63,16 +64,21 @@ if SERVER then
     hook.Add("PostEntityTakeDamage", "profiteers_hitmarkers", hitmark)
 else
     local hm = CreateClientConVar("profiteers_hitmarker_enable", "1", true, true, "Enable Profiteers Hitmarker.", 0, 3)
+    local hmpos = CreateClientConVar("profiteers_hitmarker_dynamic", "1", true, true, "Use dynamic ''real'' position for hit markers.", 0, 1)
+    local hmscale = CreateClientConVar("profiteers_hitmarker_scale", "1", true, true, "Show Longshot markers. 1 for all hits, 2 for kills only.", 0.25, 2.5)
     local indicators = CreateClientConVar("profiteers_dmgindicator_enable", "1", true, true, "Enable Profiteers Damage indicators.", 0, 1)
-    local distantshot = CreateClientConVar("profiteers_hitmarker_longshot", "1", true, true, "Show Longshot indicators. 1 for all hits, 2 for kills only.", 0, 2)
-    local hmarmor = CreateClientConVar("profiteers_hitmarker_armor", "1", true, true, "Show armor hit indicators.", 0, 1)
-    local hmhead = CreateClientConVar("profiteers_hitmarker_head", "1", true, true, "Show headshot indicators.", 0, 1)
-    local hmkill = CreateClientConVar("profiteers_hitmarker_kill", "1", true, true, "Show kill indicators.", 0, 1)
-    local hmfire = CreateClientConVar("profiteers_hitmarker_fire", "1", true, true, "Show afterburn indicators.", 0, 1)
-    local hmprop = CreateClientConVar("profiteers_hitmarker_prop", "1", true, true, "Show prop (and other breakable entity) hit indicators.", 0, 1)
+    local indicatorscale = CreateClientConVar("profiteers_dmgindicator_scale", "1", true, true, "Custom scaling for Profiteers damage indicators.", 0.25, 2.5)
+    local distantshot = CreateClientConVar("profiteers_hitmarker_longshot", "1", true, true, "Show Longshot markers. 1 for all hits, 2 for kills only.", 0, 2)
+    local hmarmor = CreateClientConVar("profiteers_hitmarker_armor", "1", true, true, "Show armor hit markers.", 0, 1)
+    local hmhead = CreateClientConVar("profiteers_hitmarker_head", "1", true, true, "Show headshot markers.", 0, 1)
+    local hmkill = CreateClientConVar("profiteers_hitmarker_kill", "1", true, true, "Show kill markers.", 0, 1)
+    local hmfire = CreateClientConVar("profiteers_hitmarker_fire", "1", true, true, "Show afterburn markers.", 0, 1)
+    local hmprop = CreateClientConVar("profiteers_hitmarker_prop", "1", true, true, "Show prop (and other breakable entity) hit markers.", 0, 1)
     local hmlength = 0.22 -- 0.5 if kill
     local lasthm = 0
     local lastdistantshot = 0
+    local lasthmpos = Vector()
+    local lasthmtbl = {x = ScrW() * 0.5, y = ScrH() * 0.5, visible = false}
     local lasthmarmor = 0
     local lasthmhead = false
     local lasthmkill = false
@@ -97,6 +103,7 @@ else
     hook.Add("PopulateToolMenu", "profiteers_hitmark_options", function()
         spawnmenu.AddToolMenuOption("Utilities", "User", "profiteers_hitmarker", "Hitmarkers", "", "", function(pan)
             pan:CheckBox("Enable directional damage indicators", "profiteers_dmgindicator_enable")
+            pan:NumSlider("Damage indicator scale", "profiteers_dmgindicator_scale", 0.25, 2.5, 3)
             pan:Help("It's those arrows pointing toward where you were shot from.")
             local mode = pan:ComboBox("Hitmarker mode", "profiteers_hitmarker_enable")
             mode:SetSortItems(false)
@@ -104,6 +111,8 @@ else
             mode:AddChoice("Audiovisual", 1)
             mode:AddChoice("Visual only", 2)
             mode:AddChoice("Audio only", 3)
+            pan:NumSlider("Hitmarker scale", "profiteers_hitmarker_scale", 0.25, 2.5, 3)
+            pan:CheckBox("Use dynamic position for hit markers", "profiteers_hitmarker_dynamic")
             local long = pan:ComboBox("Longshot indicators", "profiteers_hitmarker_longshot")
             long:SetSortItems(false)
             long:AddChoice("Disabled", 0)
@@ -116,11 +125,19 @@ else
             pan:CheckBox("Show prop (and other breakable entity) hit indicators", "profiteers_hitmarker_prop")
         end)
     end)
+
+    -- hush
+    local function DoSize(size, scale) -- scale is 2 bit operator, first bit dimension, second indicator or hitmarker
+        scale = scale or 0
+        return size * (bit.band(scale, 1) == 1 and (ScrH() / 480) or ScreenScale(1)) * (bit.band(scale, 2) == 2 and indicatorscale:GetFloat() or hmscale:GetFloat())
+    end
+
     hook.Add("HUDPaint", "profiteers_hitmark_paint", function()
         if hm:GetInt() == 3 then return end
         local lp = LocalPlayer()
         local ct = CurTime()
         local scrw, scrh = ScrW(), ScrH()
+        local pos = lasthmtbl
 
         if lasthm > ct then -- any hitmarkers
             local state = (lasthm - ct) / hmlength
@@ -140,7 +157,7 @@ else
                 surface.SetDrawColor(255, 255, 255, 255 * state)
             end
 
-            surface.DrawTexturedRect(scrw / 2 - 18 - 25 * state, scrh / 2 - 18 - 25 * state, 36 + 50 * state, 36 + 50 * state)
+            surface.DrawTexturedRect(pos.x - DoSize(6) - DoSize(8) * state, pos.y - DoSize(6) - DoSize(8) * state, DoSize(12) + DoSize(16) * state, DoSize(12) + DoSize(16) * state)
 
             if hmarmor:GetBool() and lasthmarmor > 0 then
                 surface.SetDrawColor(119, 119, 255, 255 * state)
@@ -149,17 +166,17 @@ else
                 else
                     surface.SetMaterial(matarmorb)
                 end
-                surface.DrawTexturedRect(scrw / 2 + 96, scrh / 2 - 36, 24, 24)
+                surface.DrawTexturedRect(pos.x + DoSize(16), pos.y - DoSize(12), DoSize(8), DoSize(8))
             end
             if lasthmprop then -- prop damage
                 surface.SetDrawColor(255, 255, 255, 255 * state)
                 surface.SetMaterial(matgear)
-                surface.DrawTexturedRect(scrw / 2 + 96, scrh / 2 + 12, 24, 24)
+                surface.DrawTexturedRect(pos.x + DoSize(16), pos.y + DoSize(4), DoSize(8), DoSize(8))
             end
             if hmfire:GetBool() and lasthmfire then -- afterburn damage
                 surface.SetDrawColor(255, 255, 255, 255 * state)
                 surface.SetMaterial(matfire)
-                surface.DrawTexturedRect(scrw / 2 - 12, scrh / 2 + 96, 24, 24)
+                surface.DrawTexturedRect(pos.x - DoSize(4), pos.y + DoSize(16), DoSize(8), DoSize(8))
             end
         end
 
@@ -169,17 +186,17 @@ else
             -- surface.SetFont("CGHUD_7_Shadow")
             surface.SetFont(ARC9 and "ARC9_8_Glow" or "GModNotify")
             surface.SetTextColor(0, 0, 0, 255 * state)
-            surface.SetTextPos(scrw / 2 + 75 + 1, scrh / 2 + 1)
+            surface.SetTextPos(scrw * 0.5 + DoSize(25) + 1, scrh * 0.5 + 1)
             surface.DrawText(message)
-            surface.SetTextPos(scrw / 2 + 75 + 1, scrh / 2 + 20 + 1)
+            surface.SetTextPos(scrw * 0.5 + DoSize(25) + 1, scrh * 0.5 + DoSize(7, 1) + 1)
             surface.DrawText(lasthmdistance .. " m")
             -- surface.SetFont("CGHUD_7")
             surface.SetFont(ARC9 and "ARC9_8" or "GModNotify")
             surface.SetTextColor(255, lasthmkill and 75 or 255, lasthmkill and 75 or 255, 255 * state)
-            surface.SetTextPos(scrw / 2 + 75, scrh / 2)
+            surface.SetTextPos(scrw * 0.5 + DoSize(25), scrh * 0.5)
             surface.DrawText(message)
             surface.SetTextColor(300 - 255 * (lasthmdistance / 400), 300 - 255 * (lasthmdistance / 400), 255, 255 * state)
-            surface.SetTextPos(scrw / 2 + 75, scrh / 2 + 20)
+            surface.SetTextPos(scrw * 0.5 + DoSize(25), scrh * 0.5 + DoSize(7, 1))
             surface.DrawText(lasthmdistance .. " m")
         end
 
@@ -194,7 +211,7 @@ else
                 local hitVec = v.hitvec
                 local armorBreak = v.armor
                 local ang = math.atan2(hitVec.x, hitVec.y) + math.rad(lp:EyeAngles().y) + 3.14
-                local x, y = scrw/2 + math.cos(ang) * scrh/6, scrh/2 + math.sin(ang) * scrh/6
+                local x, y = scrw/2 + math.cos(ang) * DoSize(60, 3), scrh/2 + math.sin(ang) * DoSize(60, 3)
 
                 if armorBreak > 0 then
                     surface.SetDrawColor(119, 119, 255, decay)
@@ -207,7 +224,7 @@ else
                     surface.SetDrawColor(255, 255, 255, decay)
                     surface.SetMaterial(matgothit)
                 end
-                surface.DrawTexturedRectRotated(x, y, scrh/14, scrh/14, math.deg(-ang) - 90)
+                surface.DrawTexturedRectRotated(x, y, DoSize(34, 3), DoSize(34, 3), math.deg(-ang) - 90)
             end
         end
     end)
@@ -219,6 +236,7 @@ else
         local head = net.ReadBool()
         local onfire = net.ReadBool()
         local killed = net.ReadBool()
+        local pos = net.ReadVector()
         local armored = net.ReadUInt(2)
         local distance = net.ReadUInt(16)
         local lp = LocalPlayer()
@@ -227,6 +245,8 @@ else
         lasthmhead = head
         lasthmfire = onfire
         lasthmkill = killed
+        lasthmpos = pos
+        lasthmtbl = hmpos:GetBool() and lasthmpos != Vector() and pos:ToScreen().visible and pos:ToScreen() or {x = ScrW() * 0.5, y = ScrH() * 0.5, visible = false}
         lasthmarmor = armored
         lasthmprop = !isliving
         hmlength = (armored == 2 or killed) and 0.5 or 0.22
